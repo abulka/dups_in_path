65a12c5801d2221288d9d3d294123ebcafdd7129
Andy Bulka <abulka@gmail.com>
2020-07-17 17:54:24 +1000

    cleanup

--- dups.dart
+++ dups.dart
@@ -1,14 +1,33 @@
 import 'dart:io' show Platform;
 
+/// Find duplicate PATH entries
 void main() {
-  // var s =
-  //     "/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/Users/Andy/.nvm/versions/node/v12.6.0/bin:/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/usr/local/share/dotnet:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin:/Users/andy/.local/bin:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin:/Users/andy/.local/bin:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin";
+  // test1();
+  // test2();
 
   Map<String, String> envVars = Platform.environment;
-  // print(envVars['PATH']);
-  var s = envVars['PATH'];
-  // s = "a:b:b:c";
+  report(envVars['PATH']);
+}
 
+/// test data
+test1() {
+  var s = "a:b:b:c";
+  report(s);
+}
+
+/// test 2 - why does this have so many duplicates? 
+/// I got the string from $PATH in bash
+/// Looks like running the script from terminal is different to running this script from within vscode
+/// and that this is the true situation - lots of duplicates!
+test2() {
+  var s =
+      "/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/Users/Andy/.nvm/versions/node/v12.6.0/bin:/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/usr/local/share/dotnet:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/mysql-5.7.11-osx10.9-x86_64/bin:/Users/andy/.gem/ruby/2.7.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/openssl@1.1/bin:/Users/andy/.poetry/bin:/Users/andy/.pyenv/shims:./bin:~/scripts:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin:/Users/andy/.local/bin:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin:/Users/andy/.local/bin:/Users/andy/.local/bin:/Users/andy/Devel/flutter/bin:/Users/Andy/Devel/flutter/bin/cache/dart-sdk/bin:/Users/andy/Devel/flutter/.pub-cache/bin";
+  report(s);
+}
+
+/// pass in a PATH string and it will print a flat
+/// listing one path per line, and tell you if there are duplicated
+void report(String s) {
   var lz = s.split(':');
   lz.sort();
   lz.forEach((el) => print(el));
@@ -19,41 +38,8 @@ void main() {
     duplicates.forEach((el) => print(el));
   } else
     print('** no dups');
-
-  //   print('duplicates are: ${list.toSet().toList()}');
-
-  // var lz_nodups = lz.toSet().toList();
-  // if (lz.length == lz_nodups.length)
-  //   print('no dups');
-  // else {
-  //   print(
-  //       'there are dups! lz.length=${lz.length} lz_nodups.length=${lz_nodups.length}');
-  //   // print('lz.toSet()=${lz.toSet()}');
-  //   // print('lz_nodups.toSet()=${lz_nodups.toSet()}');
-  //   // List<String> difference = lz.toSet().difference(lz_nodups.toSet()).toList();
-  //   // print(difference.toString());
-
-  //   // print(listDiff(lz, lz_nodups));
-
-  //   // List<String> output = [];
-  //   // lz.forEach((element) {
-  //   //   if (!lz_nodups.contains(element)) {
-  //   //     output.add(element);
-  //   //   }
-  //   // });
-  //   // print(output);
-
-  //   print(find_duplicates([...lz]); // send copy of lz list as param
-  //   // find_duplicates([...lz]);
-  //   // find_duplicates([...lz]);
-  // }
-
-//   print(lz);
+  print('${"-" * 20}');
 }
-
-// List<T> listDiff<T>(List<T> l1, List<T> l2) => (l1.toSet()..addAll(l2))
-//     .where((i) => !l1.contains(i) || !l2.contains(i))
-//     .toList();
 
 List find_duplicates(list) {
   // Inspired by https://stackoverflow.com/questions/59909947/finding-the-duplicated-values-in-list-of-string-dart
